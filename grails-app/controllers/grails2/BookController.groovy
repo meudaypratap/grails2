c2dd67ecf32b5fabe2cb31b25dc08d2bb4f71985
Uday Pratap Singh <uday@intelligrape.com>
2012-02-14 23:26:39 +0530

    Added where query examples

--- grails-app/controllers/grails2/BookController.groovy
+++ grails-app/controllers/grails2/BookController.groovy
@@ -4,4 +4,20 @@ class BookController {
     static layout = 'book'
     def scaffold = Book
 
+    def where() {
+        def query = Book.where {
+            price > 30f || author == Author.get(2)
+        }
+//        render query.findAllByNameIlike("Book_1%")
+//        render query.list([max: 5,sort: 'id'])
+//        render query.find()
+//        render query.findAll()
+    }
+
+    def range() {
+        def query = Book.where {
+            price in (20f..30f)
+        }
+        render query.list()
+    }
 }
