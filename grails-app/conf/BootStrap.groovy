1eef92e417601195f29fe1a530d16857bf54a229
Uday Pratap Singh <uday@intelligrape.com>
2012-02-14 23:10:43 +0530

    Added dummy data in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,23 @@
+import grails2.Author
+import grails2.Book
+
 class BootStrap {
 
     def init = { servletContext ->
 
+        (1..10).each {
+            Author author = new Author(name: "Author_${it}", dob: (new Date() - (it * 30)))
+            author.save()
+        }
+
+        Author.list().each {Author author ->
+            (1..5).each {
+                Book book = new Book(name: "Book_${(author.id * 10) + it}", price: ((author.id * 10) + it))
+                author.addToBooks(book)
+                author.save()
+            }
+
+        }
     }
     def destroy = {
     }
