f123878a205b03ec2d657572542cebd8647676a1
Uday Pratap Singh <uday@intelligrape.com>
2012-11-08 01:05:08 +0530

    Added bootstrap css

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,7 +33,8 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 
 // What URL patterns should be processed by the resources plugin
 grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
-bootstrap.theme = "united"  //amelia,cerulean,united
+//amelia,cerulean,united,spacelab,spruce  http://bootswatch.com/
+bootstrap.theme = "spruce"
 
 //grails.sitemesh.default.layout = 'book'
 // The default codec used to encode data with ${}
